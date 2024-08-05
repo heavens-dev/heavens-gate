2155f6c5df19c8f07905a621dde8afe6cdf08a24
CreativeeBlackWolf <maksim271202@gmail.com>
2024-08-05 15:15:17 +0300

    alter enums

--- core/db/enums.py
+++ core/db/enums.py
@@ -3,4 +3,6 @@ from enum import Enum
 
 class StatusChoices(Enum):
     STATUS_CREATED = 0
-    STATUS_BLOCKED = 1
+    STATUS_IP_BLOCKED = 1
+    STATUS_ACCOUNT_BLOCKED = 2
+    STATUS_TIME_EXPIRED = 3
