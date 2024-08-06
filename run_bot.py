7acddfbbd4924428e015f03ddfd1b51ca72feab1
CreativeeBlackWolf <maksim271202@gmail.com>
2024-08-06 15:45:36 +0300

    uh-oh.

--- run_bot.py
+++ run_bot.py
@@ -7,7 +7,6 @@ def main() -> None:
     bot_instance = bot.get_bot(bot_cfg.token)
     dispatcher = bot.prepare_bot(bot_instance)
     asyncio.run(dispatcher.run_polling(bot_instance))
-    int()
 
 if __name__ == "__main__":
     main()
