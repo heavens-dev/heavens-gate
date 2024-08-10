f07682af569f255789fa62c697a7b58e4e1b72be
CreativeeBlackWolf <maksim271202@gmail.com>
2024-08-10 12:44:49 +0300

    rewrite admin handler

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -2,7 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Command
 from config.loader import bot_cfg
-from core.db.db_works import Users
+from core.db.db_works import ClientFactory
 import sys
 import os
 
@@ -11,10 +11,6 @@ admin_router = Router()
 admin_router.message.filter(
     F.from_user.id.in_(bot_cfg.admins)
 )
-
-@admin_router.message(Command("admin_help"))
-async def admin_help(message: Message):
-    await message.answer("Сам справишься.")
 
 @admin_router.message(Command("reboot"))
 async def reboot(message: Message) -> None:
@@ -30,15 +26,16 @@ async def reboot(message: Message) -> None:
 @admin_router.message(Command("broadcast"))
 async def broadcast(message: Message):
     """Broadcast message to ALL registered users"""
-    if len(message.text.split()) <= 1:
-        await message.answer("Сообщение должно содержать хотя бы какой-то текст для отправки.")
+    args = message.text.split()
+    if len(args) <= 1:
+        await message.answer("❌ Сообщение должно содержать хотя бы какой-то текст для отправки.")
         return
-    all_users = Users.get_users()
+    all_clients = ClientFactory.select_clients()
     text = "✉️ <b>Рассылка от администрации</b>:\n\n"
-    text += " ".join(message.text.split()[1::])
+    text += " ".join(args[1::])
     # ? sooo we can get rate limited probably. fixme? maybe later.
-    for user in all_users:
-        if message.chat.id == user.telegram_id:
+    for client in all_clients:
+        if message.chat.id == client.userdata.telegram_id:
             continue
-        await message.bot.send_message(user.telegram_id, text, parse_mode="HTML")
+        await message.bot.send_message(client.userdata.telegram_id, text, parse_mode="HTML")
     await message.answer("Сообщение транслировано всем пользователям.")
