1497d15f5fb09e3e4061028476d89c6a8a1deffa
sychikov <passiveincubus@gmail.com>
2024-08-18 16:48:11 +0300

    Base for wg_work

---
+++ core/wg/wg_work.py
@@ -0,0 +1,26 @@
+import subprocess
+import keygen
+from core.db.model_serializer import ConnectionPeer
+from config.loader import server_cfg
+
+def create_peer_keys():
+    pass
+
+def setup_server_keys():
+    pass
+
+def create_server_base():
+    pass
+
+def add_peer():
+    pass
+
+def delete_peer():
+    pass
+
+def disable_server():
+    pass
+
+def enable_server():
+    pass
+
