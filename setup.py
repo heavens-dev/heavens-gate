c8296fd6cd94969e974457937e7e0b97a4ac2f68
sychikov <passiveincubus@gmail.com>
2024-08-20 10:29:32 +0300

    Fix setup script. Add ip address check

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from core.wg.keygen import generate_private_key, generate_public_key
 from configparser import ConfigParser
 import os
 from requests import get
+from core.utils.check import check_ip_address
 
 def make_config(path):
     #Create pair of crypto keys
@@ -39,7 +40,12 @@ def get_ip_range():
     if not ip_range or ip_range == "y" or ip_range == "Y":
         return "10.28.98"
     else:
-        return input("[ ! ] Your IP range ('0.0.0'): ")
+        ip_range = input("[ ! ] Your IP range ('0.0.0'): ")
+        if check_ip_address(ip_range + ".1"):
+            return ip_range
+        else:
+            print("[ - ] Invalid IP range")
+            return get_ip_range()
 
 def get_endpoint_port():
     ip_range = input("[ ? ] Are you okay with 54817 (recommended one) port for your Wireguard Service IP? Y/n ")
