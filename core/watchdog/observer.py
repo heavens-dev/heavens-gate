981686be15b0b385ef9d641b9201457eb5c8bb7e
head47 <63517545+head47@users.noreply.github.com>
2024-08-27 17:18:57 +0300

    this -> these

--- core/watchdog/observer.py
+++ core/watchdog/observer.py
@@ -27,7 +27,7 @@ class EventObserver:
                 if not any(tuple(i for i in self.required_types if i == argtype)):
                     warnings.warn(
                         message=f"Ensure that function '{fn.__name__}' correctly handles "
-                        f"argument '{argname}' for any of this types: {self.required_types}. "
+                        f"argument '{argname}' for any of these types: {self.required_types}. "
                         "Add correct annotation to hide this warning",
                         stacklevel=3
                     )
