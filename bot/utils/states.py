0bf380ec0932581e022bbb63feafc490cdc8ca4c
CreativeeBlackWolf <maksim271202@gmail.com>
2024-11-05 02:28:39 +0300

    add changing peer name ability and buttons to me command

--- bot/utils/states.py
+++ bot/utils/states.py
@@ -3,3 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class PreviewMessageStates(StatesGroup):
     preview = State()
+
+class RenamePeerStates(StatesGroup):
+    peer_selection = State()
+    name_entering = State()
